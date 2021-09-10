6dcd578f7e63004654d259af5382944ef11d5f1a
Max Gotenstam <maxrsg@gmail.com>
2021-09-10 17:09:34 +0200

    Add rest of CRUD functionality

--- models/docdata.js
+++ models/docdata.js
@@ -8,6 +8,7 @@ const docs = {
     try {
       db = await database.getDb();
       const resultSet = await db.collection.find({}).toArray();
+
       return res.status(200).json({
         data: resultSet,
       });
@@ -35,6 +36,7 @@ const docs = {
       try {
         db = await database.getDb();
         const object = await db.collection.findOne(filter);
+
         return res.status(200).json({
           data: object,
         });
@@ -55,6 +57,111 @@ const docs = {
         error: {
           status: 500,
           path: "/docs/one no id",
+          title: "No id",
+          message: "No data id provided",
+        },
+      });
+    }
+  },
+  createDoc: async function (res, req) {
+    let db;
+
+    try {
+      db = await database.getDb();
+      const doc = {
+        title: req.body.title,
+        data: req.body.data,
+      };
+      const result = await db.collection.insertOne(doc);
+
+      return res.status(201).json({ data: result.ops });
+    } catch (e) {
+      return res.status(500).json({
+        error: {
+          status: 500,
+          path: "POST /docs",
+          title: "Database error",
+          message: e.message,
+        },
+      });
+    } finally {
+      await db.client.close();
+    }
+  },
+
+  updateDoc: async function (res, req) {
+    if (req.body.id) {
+      let _id = req.body.id;
+      let filter = {
+        _id: ObjectId(_id),
+      };
+      let db;
+
+      try {
+        db = await database.getDb();
+        const updateDoc = {
+          title: req.body.title,
+          data: req.body.data,
+        };
+        await db.collection.updateOne(filter, {
+          $set: updateDoc,
+        });
+
+        return res.status(201).send();
+      } catch (e) {
+        return res.status(500).json({
+          error: {
+            status: 500,
+            path: "PUT /docs",
+            title: "Database error",
+            message: e.message,
+          },
+        });
+      } finally {
+        await db.client.close();
+      }
+    } else {
+      return res.status(500).json({
+        error: {
+          status: 500,
+          path: "PUT /docs no id",
+          title: "No id",
+          message: "No data id provided",
+        },
+      });
+    }
+  },
+
+  deleteDoc: async function (res, req) {
+    if (req.body.id) {
+      let _id = req.body.id;
+      let filter = {
+        _id: ObjectId(_id),
+      };
+      let db;
+
+      try {
+        db = await database.getDb();
+        await db.collection.deleteOne(filter);
+
+        return res.status(201).send();
+      } catch (e) {
+        return res.status(500).json({
+          error: {
+            status: 500,
+            path: "DELETE /docs",
+            title: "Database error",
+            message: e.message,
+          },
+        });
+      } finally {
+        await db.client.close();
+      }
+    } else {
+      return res.status(500).json({
+        error: {
+          status: 500,
+          path: "DELETE /docs no id",
           title: "No id",
           message: "No data id provided",
         },
--- routes/docs.js
+++ routes/docs.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const docs = require("../models/docdata.js");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 // get all documents
 router.get("/", (req, res) => docs.getAllDocs(res, req));
 
 // get specific document by id
 router.get("/:id", (req, res) => docs.getOneDoc(res, req.params.id));
+
+// create a new document
+router.post("/", jsonParser, (req, res) => docs.createDoc(res, req));
+
+// update existing document
+router.put("/", jsonParser, (req, res) => docs.updateDoc(res, req));
+
+// delete existing document
+router.delete("/", jsonParser, (req, res) => docs.deleteDoc(res, req));
 
 module.exports = router;
